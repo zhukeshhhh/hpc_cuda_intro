11c7e09879f0849c8879c9c7b45e46d4aaf3f961
nurzhan <nurzhan.zhukesh@gmail.com>
2025-07-14 08:06:12 +0200

    tiled matmul implementation

--- prep/tiled_matmul.cu
+++ prep/tiled_matmul.cu
@@ -1,53 +1,129 @@
-#include <stdio.h>
-#include <math.h>
+// This program computes matrix multiplication using shared memory tiling
+// By: Nick from CoffeeBeforeArch
 
-__global__ void tiledMatMul(float *a, float *b, float *c, int n, int tile_size) {
-    // two statically-sized pieces of shared memory
-    
-}
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <vector>
 
-void mat_init(float* mat, int size) {
-    for (int i = 0; i < size * size; i++) {
-        mat[i] = 1.0;
+using std::cout;
+using std::generate;
+using std::vector;
+
+// Pull out matrix and shared memory tile size 
+const int N = 1 << 10;
+const int SHMEM_SIZE = 1 << 10;
+
+__global__ void matrixMul(const int *a, const int *b, int *c) {
+  // Compute each thread's global row and column index
+  int row = blockIdx.y * blockDim.y + threadIdx.y;
+  int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+  // Statically allocated shared memory
+  __shared__ int s_a[SHMEM_SIZE];
+  __shared__ int s_b[SHMEM_SIZE];
+
+  // Accumulate in temporary variable
+  int tmp = 0;
+
+  // Sweep tile across matrix
+  for (int i = 0; i < N; i += blockDim.x) {
+    // Load in elements for this tile
+    s_a[threadIdx.y * blockDim.x + threadIdx.x] = a[row * N + i + threadIdx.x];
+    s_b[threadIdx.y * blockDim.x + threadIdx.x] =
+        b[i * N + threadIdx.y * N + col];
+
+    // Wait for both tiles to be loaded in before doing computation
+    __syncthreads();
+
+    // Do matrix multiplication on the small matrix
+    for (int j = 0; j < blockDim.x; j++) {
+      tmp +=
+          s_a[threadIdx.y * blockDim.x + j] * s_b[j * blockDim.x + threadIdx.x];
     }
+
+    // Wait for all threads to finish using current tiles before loading in new
+    // ones
+    __syncthreads();
+  }
+
+  // Write back results
+  c[row * N + col] = tmp;
 }
 
+// Check result on the CPU
+void verify_result(vector<int> &a, vector<int> &b, vector<int> &c) {
+  // For every row...
+  for (int i = 0; i < N; i++) {
+    // For every column...
+    for (int j = 0; j < N; j++) {
+      // For every element in the row-column pair
+      int tmp = 0;
+      for (int k = 0; k < N; k++) {
+        // Accumulate the partial results
+        tmp += a[i * N + k] * b[k * N + j];
+      }
+
+      // Check against the CPU result
+      assert(tmp == c[i * N + j]);
+    }
+  }
+}
 
 int main() {
+  // Size (in bytes) of matrix
+  size_t bytes = N * N * sizeof(int);
 
-    int n = 1 << 10;
+  // Host vectors
+  vector<int> h_a(N * N);
+  vector<int> h_b(N * N);
+  vector<int> h_c(N * N);
 
-    size_t bytes = n * n * sizeof(float);
+  // Initialize matrices
+  generate(h_a.begin(), h_a.end(), []() { return rand() % 100; });
+  generate(h_b.begin(), h_b.end(), []() { return rand() % 100; });
 
-    float *h_a, *h_b, *h_c;
+  // Allocate device memory
+  int *d_a, *d_b, *d_c;
+  cudaMalloc(&d_a, bytes);
+  cudaMalloc(&d_b, bytes);
+  cudaMalloc(&d_c, bytes);
 
-    h_a = (float*)malloc(bytes);
-    h_b = (float*)malloc(bytes);
-    h_c = (float*)malloc(bytes);
+  // Copy data to the device
+  cudaMemcpy(d_a, h_a.data(), bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, h_b.data(), bytes, cudaMemcpyHostToDevice);
 
-    float *d_a, *d_b, *d_c;
+  // Threads per CTA dimension
+  int THREADS = 32;
 
-    cudaMalloc(&d_a, bytes);
-    cudaMalloc(&d_b, bytes);
-    cudaMalloc(&d_c, bytes);
-    
-    dim3 THREADS(32, 32);
-    dim3 BLOCKS((n + 31) / 32, (n + 31) / 32);
+  // Blocks per grid dimension (assumes THREADS divides N evenly)
+  int BLOCKS = N / THREADS;
 
+  // Use dim3 structs for block  and grid dimensions
+  dim3 threads(THREADS, THREADS);
+  dim3 blocks(BLOCKS, BLOCKS);
 
-    mat_init(h_a, n);
-    mat_init(h_b, n);
+  // Launch kernel
+  matrixMul<<<blocks, threads>>>(d_a, d_b, d_c);
 
-    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
+  // Copy back to the host
+  cudaMemcpy(h_c.data(), d_c, bytes, cudaMemcpyDeviceToHost);
 
-    matmul_parallel_square<<<BLOCKS, THREADS>>>(d_a, d_b, d_c, n);
+  // Check result
+  verify_result(h_a, h_b, h_c);
 
-    cudaMemcpy(h_c, d_c, bytes, cudaMemcpyDeviceToHost);
-
-    for(int i = 0; i < 1500; i++) {
-        printf("c[%d] = %f\n", i, h_c[i]);
+  for(int i = 0; i < 1500; i++) {
+        printf("c[%d] = %d\n", i, h_c[i]);
     }
 
-    return 0;
+  cout << "COMPLETED SUCCESSFULLY\n";
+
+  // Free memory on device
+  cudaFree(d_a);
+  cudaFree(d_b);
+  cudaFree(d_c);
+
+  return 0;
 }
